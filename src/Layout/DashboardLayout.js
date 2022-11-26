69e17b3cd6a54a465821f75c5cb124817eb723ff
sadiq1020 <sajidnsubd@gmail.comm>
2022-11-27 03:25:58 +0600

    buyer an seller checking from db

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider';
+import useAdmin from '../hooks/useAdmin';
+import useBuyer from '../hooks/useBuyer';
+import useSeller from '../hooks/useSeller';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
+    const { user } = useContext(AuthContext);
+
+    const [isAdmin] = useAdmin(user?.email);
+    const [isSeller] = useSeller(user?.email);
+    const [isBuyer] = useBuyer(user?.email);
+
     return (
         <div>
             <Navbar></Navbar>
@@ -17,14 +27,30 @@ const DashboardLayout = () => {
                 <div className="drawer-side">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-                        {/* <!-- Sidebar content here --> */}
-                        <li className='mt-2'><Link to='/dashboard/myorders'>My Orders</Link></li>
-                        <li className='mt-3'><Link to='/dashboard/addproduct'>Add Product</Link></li>
-                        <li className='mt-3'><Link to='/dashboard/myproducts'>My Products</Link></li>
-                        <li className='mt-3'><Link to='/dashboard/allsellers'>All Sellers</Link></li>
-                        <li className='mt-3'><Link to='/dashboard/allbuyers'>All Buyers</Link></li>
+                        {
+                            isAdmin &&
+                            <>
+                                <li className='mt-2'><Link to='/dashboard/myorders'>My Orders</Link></li>
+                                <li className='mt-3'><Link to='/dashboard/addproduct'>Add Product</Link></li>
+                                <li className='mt-3'><Link to='/dashboard/myproducts'>My Products</Link></li>
+                                <li className='mt-3'><Link to='/dashboard/allsellers'>All Sellers</Link></li>
+                                <li className='mt-3'><Link to='/dashboard/allbuyers'>All Buyers</Link></li>
+                            </>
+                        }
+                        {
+                            isSeller &&
+                            <>
+                                <li className='mt-3'><Link to='/dashboard/addproduct'>Add Product</Link></li>
+                                <li className='mt-3'><Link to='/dashboard/myproducts'>My Products</Link></li>
+                            </>
+                        }
+                        {
+                            isBuyer &&
+                            <>
+                                <li className='mt-2'><Link to='/dashboard/myorders'>My Orders</Link></li>
+                            </>
+                        }
                     </ul>
-
                 </div>
             </div>
         </div>
